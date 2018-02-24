861c55832b82ff30562cfc3ed9b5fd39c9de80c0
sfletche <sfletche@gmail.com>
2018-02-23 17:45:40 -0800

    remove extension from file name and replace tabs with spaces

--- src/get-content.js
+++ src/get-content.js
@@ -2,7 +2,7 @@
 function getPropString(propData) {
   const { name, value } = propData;
   const actualValue = value.includes('"') ? value : `props.${value}`;
-  return `\n\t\t\t\t${name}: ${actualValue}`;
+  return `${name}: ${actualValue}`;
 }
 
 function getComponentTest(renderedComponent) {
@@ -11,7 +11,8 @@ function getComponentTest(renderedComponent) {
   return `
   describe('${componentName}', () => {
     it('is rendered with props', () => {
-      const expectedProps = {${props.map(getPropString)}
+      const expectedProps = {
+        ${props.map(getPropString)}
       };
       expect(component.find('${componentName}').props()).toEqual(expectedProps);
     });
@@ -43,16 +44,17 @@ function getPropValue(propType) {
 function getPropPairs(propData) {
   const { propName, propType } = propData;
   const propValue = getPropValue(propType);
-  return `\n\t\t${propName}: ${propValue}`;
+  return `\n    ${propName}: ${propValue}`;
 }
 
 function getContent({ pathToComponent, componentName, componentProps, renderedComponents }) {
+  const pathToComponentMinusExt = pathToComponent.replace(/\.[^/.]+$/, '')
   return `
 import React from 'react';
 import { shallow } from 'enzyme';
 // TODO: import from path needs to be fixed
 // determine the depth of the dest path and apply to the from path
-import ${componentName} from '../${pathToComponent}';
+import ${componentName} from '../${pathToComponentMinusExt}';
 
 describe('${componentName}', () => {
   const props = {${componentProps.map(getPropPairs)},
