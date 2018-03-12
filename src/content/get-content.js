37bc047627b28edb4ebe1dabaa983a9e6551ad93
sfletche <sfletche@gmail.com>
2018-03-12 09:36:27 -0700

    better handling of duplicate path arguments (path to component / path to test)

--- src/content/get-content.js
+++ src/content/get-content.js
@@ -8,27 +8,40 @@ function getMomentImport(props) {
   }
 }
 
-function getRelativeLocation(pathToComponent, pathToUnitTest) {
-  if (pathToComponent.indexOf(pathToUnitTest) === 0) {
+function getPathToComponent(pathToComponent, pathToUnitTest) {
+  const pathToComponentMinusExt = pathToComponent.replace(/\.[^/.]+$/, '');
+  if (pathToComponentMinusExt.indexOf(pathToUnitTest) === 0) {
     return './';
   }
+  // handling path comparisons similar to the following
+  // src/application/provide-documentation/components/emergency-hvac-verification.jsx
+  // src/application/provide-documentation/__tests__/components
+  // (and ignoring the first 3 segments as they are duplicates of each other)
+  const componentPathSegments = pathToComponentMinusExt.split('/');
   const testPathSegments = pathToUnitTest.split('/');
-  return '../'.repeat(testPathSegments.length);
+  const dupes = _.reduce(componentPathSegments, (count, segment, index) => {
+    count += segment === testPathSegments[index] ? 1 : 0;
+    return count;
+  }, 0);
+
+  const pathToComponentWithoutDupes = componentPathSegments.slice(dupes).join('/');
+  const relativeLocation = '../'.repeat(testPathSegments.length - dupes);
+  return `${relativeLocation}${pathToComponentWithoutDupes}`;
 }
 
 function getContent({ pathToComponent, pathToUnitTest, componentName, componentProps, renderedComponents }) {
-  const pathToComponentMinusExt = pathToComponent.replace(/\.[^/.]+$/, '');
   const hasMoment = _.some(componentProps, ['propType', 'instanceOf(Date)']);
-  const relativeLocation = getRelativeLocation(pathToComponent, pathToUnitTest);
+  const relativePathToComponent = getPathToComponent(pathToComponent, pathToUnitTest);
+
   return `import React from 'react'; ${hasMoment ? `\nimport moment from 'moment';` : ``}
 import { shallow } from 'enzyme';
-import ${componentName} from '${relativeLocation}${pathToComponentMinusExt}';
+import ${componentName} from '${relativePathToComponent}';
 
 describe('${componentName}', () => {
   ${getPropsDeclaration(componentProps)}let component;
 
   beforeEach(() => {
-    component = shallow(<${componentName} {...props} />);
+    component = shallow(<${componentName}${componentProps.length ? ` {...props}` : ``} />);
   });
   ${renderedComponents.map(getComponentTest).join('')}
 });
